cee8354535587426766d4bac7973170b96effbef
teena-morin502 <teenamorin2212@gmail.com>
2024-09-03 13:39:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+let principle=10000;
+let rate=5;
+let time=3;
+console.log((principle*rate*time)/100);
+ 
+let r=14;
+let pi=3.14159;
+console.log(pi*r**2);
+
+let a=150;
+let b=200;
+let c=a;
+a=b;
+b=c;
+console.log(a,b);
+
+let std1=85;
+let std2=90;
+let std3=95;
+console.log(std1+std2+std3/3);
+
+let p=5000;
+ r=6;
+ let t=4;
+ console.log(p*(1+r/100)^t-p);
+
+ let cp=800;
+ let sp=950;
+ console.log(sp-cp);
+
+ let length=50;
+ let width=30;
+ console.log(2*(length+width));
+
+ let x=25;
+ let y=19;
+ console.log(x&y);
+
+ let celsius=22;
+ console.log(celsius*9/5);
+
+ r=5;
+ let h=10;
+ console.log(pi*r**2*h);
+
+ let char = 'A';
+
+if (char === 'a') {
+  console.log('The character is a.');
+} else if (char === 'b') {
+  console.log('The character is b.');
+} else {
+  console.log('The character is something else.');
+}
+const value = null;
+
+if (value) {
+    console.log("Value is truthy.");
+} else {
+    console.log("Value is falsy.");
+}
+
+if(isEven(2)){console.log("even")}
+else{console.log("not even")}
+if(isEven(5)){console.log("even")} 
+else{console.log("not even")}
