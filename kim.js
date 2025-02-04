8d0d413bf1deb1e8f9cdd33ab2abd7a995bed30e
teena-morin502 <teenamorin2212@gmail.com>
2025-02-04 12:21:26 +0530

    altered

--- kim.js
+++ kim.js
@@ -1,6 +1,8 @@
-let fruits=["apple","grapes","mango","kiwi"]
-let i=0;
-while(i<=fruits.length-1){
-    console.log(fruits[i]);
-    i++
+function leapYear(year){
+    if(year%4==0 && year%100!==0 || year%400==0){
+        console.log(year+" is a leap year")
+    }else{
+        console.log(year+" is not a leap year")
+    }
 }
+leapYear(1700)
